0877580ca340f876d304fe8b6ffd263d2b961aa4
Jean-Baptiste Potonnier <jb@dune.io>
2014-09-23 23:14:58 +0200

    format

--- heavy-rabbit.elm
+++ heavy-rabbit.elm
@@ -1,27 +1,21 @@
--- See this document for more information on making heavy-duck:
--- https://github.com/dboissier/workshops/blob/master/elm/heavy-duck.elm
-
 import Keyboard
 import Text
 
--- Inputs
-
-
--- Model
 rabbitImg = image 80 100 "http://www.canardpc.com/img/couly/img141.png"
 
--- Updates
-
-
-
--- Display
 bgBlue = rgb 100 220 255
 rectWidth  = 320
 recHeight = 200
 
 format_direction : {x: Int, y: Int} -> String
-format_direction direction =
-     "KB direction: " ++ show direction.x ++ ", " ++ show direction.y ++ "}"
+format_direction direction = 
+    concat [
+           "KB direction: {"
+         , show direction.x 
+         , ", "
+         , show direction.y 
+         , "}"
+    ]
 
 display : {x: Int, y: Int} -> Element
 display direction = collage rectWidth recHeight [
