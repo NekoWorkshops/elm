af2871442af3dce13aaf8e9b26f91eafb250df7e
Jean-Baptiste Potonnier <jb@dune.io>
2014-09-24 00:31:24 +0200

    move rabbit

--- heavy-rabbit.elm
+++ heavy-rabbit.elm
@@ -2,9 +2,20 @@ import Keyboard
 import Window
 import Text
 
--- Model
-rabbit = { x=0, y=0 }
+type Position = (Int, Int)
 
+type Direction = {x:Int, y:Int}
+
+model = {
+  direction = { x=0, y=0 }
+  , rabbit = (0, 0)
+  }
+  
+walk dir m = 
+    let (x, y) = m.rabbit
+    in { m | direction <- dir
+           , rabbit <- (x + dir.x, y) }
+                 
 -- Display
 rabbitImg = image 80 100 "http://www.canardpc.com/img/couly/img141.png"
 
@@ -13,21 +24,22 @@ bgBlue = rgb 100 220 255
 formatDirection : {x: Int, y: Int} -> String
 formatDirection direction = 
     concat [
-           "KB direction: {"
+           "Arrows direction: {"
          , show direction.x 
          , ", "
          , show direction.y 
          , "}"
     ]
 
-render: (Int, Int) -> {x: Int, y: Int} -> Element
-render (width, heigth) direction = collage width heigth [
+render: (Int, Int) -> {direction:Direction, rabbit:Position} -> Element
+render (width, heigth) model = collage width heigth [
              filled bgBlue <| rect (toFloat width) (toFloat heigth)
-           , toForm rabbitImg
-           , move (0, 80) <| toForm <| leftAligned <| toText <| formatDirection direction
+           , moveX (toFloat <| fst <| model.rabbit) <| toForm rabbitImg
+           , move (0, 80) <| toForm <| leftAligned <| toText <| formatDirection model.direction
        ]
 
+input : Signal Direction
 input = Keyboard.arrows
        
 main : Signal Element
-main = lift2 render Window.dimensions input
+main = lift2 render Window.dimensions (foldp walk model input)
